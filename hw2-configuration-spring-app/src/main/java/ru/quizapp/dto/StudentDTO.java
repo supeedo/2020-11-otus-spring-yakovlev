d1ac8005f719c8d7582384c72fdd2adb2999e520
Sergey Yakovlev <web-wind@ya.ru>
2020-12-06 12:43:29 +0300

    create student

---
+++ hw2-configuration-spring-app/src/main/java/ru/quizapp/dto/StudentDTO.java
@@ -0,0 +1,18 @@
+package ru.quizapp.dto;
+
+public class StudentDTO {
+
+    private final String firstName;
+    private final String lastName;
+
+    public StudentDTO(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Студент: " +
+                firstName + " " + lastName;
+    }
+}
