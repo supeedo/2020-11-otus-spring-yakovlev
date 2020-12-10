eae669e03de91781b2402be60f3b5fb3db7bb79f
Sergey Yakovlev <web-wind@ya.ru>
2020-12-10 16:10:11 +0300

    refactor exceptions

--- hw2-configuration-spring-app/src/test/java/ru/quizapp/service/ExamTicketServiceTest.java
+++ hw2-configuration-spring-app/src/test/java/ru/quizapp/service/ExamTicketServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
@@ -15,13 +14,16 @@ import org.mockito.quality.Strictness;
 import ru.quizapp.dto.ExamTicketDTO;
 import ru.quizapp.dto.ExaminationDTO;
 import ru.quizapp.dto.StudentDTO;
+import ru.quizapp.exceptions.ResourceException;
 import ru.quizapp.repository.ExamTicketRepository;
 import ru.quizapp.utils.ConsoleHelper;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import static ru.quizapp.exceptions.ResourceException.ErrorCode.CONSOLE_READING_ERROR;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -35,14 +37,17 @@ class ExamTicketServiceTest {
     @Mock
     private ExamTicketRepository repository;
 
+    @Mock
+    private ConsoleHelper consoleHelper;
+
     private ExamTicketService service;
 
     private StudentDTO student;
 
-
     @BeforeEach
     public void setUp() {
-       // this.service = new ExamTicketServiceImpl(repository);
+        consoleHelper = Mockito.mock(ConsoleHelper.class);
+        this.service = new ExamTicketServiceImpl(repository, consoleHelper);
         questionDTOList.add(new ExamTicketDTO.ExamTicketDTOBuilder()
                 .setQuestion("Tests mock questions")
                 .setAnswers(new HashMap<>() {{
@@ -69,22 +74,26 @@ class ExamTicketServiceTest {
     @Test
     @DisplayName("Check receipt studentDTO")
     public void studentRegistration() {
-        try (MockedStatic<ConsoleHelper> dummy = Mockito.mockStatic(ConsoleHelper.class)) {
-         //   dummy.when(ConsoleHelper::readString).thenReturn(testFirstName, testLastName);
+        try {
+            Mockito.when(consoleHelper.readString()).thenReturn(testFirstName, testLastName);
             StudentDTO studentDTO = service.studentRegistration();
             Assertions.assertNotNull(studentDTO);
             Assertions.assertEquals(studentDTO.getFirstName(), testFirstName, "FirstName разные");
             Assertions.assertEquals(studentDTO.getLastName(), testLastName, "LastName разные");
+        } catch (IOException error) {
+            throw new ResourceException("Error reading data from console", error, CONSOLE_READING_ERROR);
         }
     }
 
     @Test
     public void studentTesting() {
-        try (MockedStatic<ConsoleHelper> dummy = Mockito.mockStatic(ConsoleHelper.class)) {
-         //   dummy.when(ConsoleHelper::readString).thenReturn("1");
+        try {
+            Mockito.when(consoleHelper.readString()).thenReturn("1");
             ExaminationDTO dummyExam = new ExaminationDTO(student, 1, 1);
-            ExaminationDTO examinationDTO = service.studentTesting(student);
+            ExaminationDTO examinationDTO = this.service.studentTesting(student);
             Assertions.assertNotNull(examinationDTO);
+        } catch (IOException error) {
+            throw new ResourceException("Error reading data from console", error, CONSOLE_READING_ERROR);
         }
     }
 
