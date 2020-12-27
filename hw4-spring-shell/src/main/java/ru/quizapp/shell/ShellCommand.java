2aa6bf1bf30190bc47d0f4982c6dc27a382f8792
Sergey Yakovlev <web-wind@ya.ru>
2020-12-27 15:22:26 +0300

    edit shell descriptions

--- hw4-spring-shell/src/main/java/ru/quizapp/shell/ShellCommand.java
+++ hw4-spring-shell/src/main/java/ru/quizapp/shell/ShellCommand.java
@@ -11,55 +11,70 @@ import ru.quizapp.dto.ExaminationDTO;
 import ru.quizapp.dto.StudentDTO;
 import ru.quizapp.service.ExamTicketService;
 import ru.quizapp.service.StudentRegistrationService;
+import ru.quizapp.utils.LocaleDataHelper;
 
 @ShellComponent
 public class ShellCommand {
 
     private static final Logger logger = LoggerFactory.getLogger(ShellCommand.class);
+
     private final ExamTicketService questionService;
     private final StudentRegistrationService studentRegistration;
+    private final LocaleDataHelper localeDataHelper;
+
     private StudentDTO student;
     private ExaminationDTO examination;
 
     @Autowired
-    public ShellCommand(ExamTicketService questionService, StudentRegistrationService studentRegistration) {
+    public ShellCommand(ExamTicketService questionService, StudentRegistrationService studentRegistration, LocaleDataHelper localeDataHelper) {
+        logger.info("Class initialization");
         this.questionService = questionService;
         this.studentRegistration = studentRegistration;
+        this.localeDataHelper = localeDataHelper;
     }
 
-    @ShellMethod(value = "Регистрация студента", key = {"reg", "registration"})
-    public String register(){
-         this.student = studentRegistration.studentRegistration();
-         return "Приветствие";
+    @ShellMethod(value = "Student registration", key = {"reg", "registration"})
+    public String register() {
+        this.student = studentRegistration.studentRegistration();
+        logger.info("Student registered = {}", student);
+        return String.format(localeDataHelper.getLocaleMessage("shell.greeting"), student);
     }
 
-    @ShellMethod(value = "Проведение экзамена", key = {"start", "exam"})
-    public String exam(){
-         this.examination = questionService.studentTesting(student);
-         return "Окончание экзамена";
+    @ShellMethod(value = "Taking an exam", key = {"start", "exam"})
+    public String exam() {
+        this.examination = questionService.studentTesting(student);
+        logger.info("Testing is over = {}", examination);
+        return localeDataHelper.getLocaleMessage("shell.exam.finish");
     }
 
-    @ShellMethod(value = "Результаты проведенного экзамена", key = {"result", "outcome"})
-    public String result(){
+    @ShellMethod(value = "Results of the conducted exam", key = {"result", "outcome"})
+    public String result() {
+        logger.info("Output of test results");
         questionService.resultsOfTheConductedTesting(examination);
-        return "Exit";
+        return localeDataHelper.getLocaleMessage("shell.exam.result");
     }
 
     @ShellMethodAvailability({"start", "exam"})
-    public Availability isExamAvailable(){
-      if(student != null){
-          return Availability.available();
-      }else{
-          return Availability.unavailable("Для начала тестирования необходимо зарегистрироваться!");
-      }
+    public Availability isExamAvailable() {
+        logger.info("Verification that the student is registered and testing can begin");
+        if (student != null) {
+            logger.debug("You can start the exam");
+            return Availability.available();
+        } else {
+            logger.debug("Student registration failed");
+            return Availability.unavailable(localeDataHelper.getLocaleMessage("shell.error.exam"));
+        }
     }
 
     @ShellMethodAvailability({"result", "outcome"})
-    public Availability isResultAvailable(){
-        if(examination != null){
+    public Availability isResultAvailable() {
+        logger.info("Checking that testing has been carried out and you can display the result");
+        if (examination != null) {
+            logger.debug("Testing is over, the result can be displayed");
             return Availability.available();
-        }else{
-            return Availability.unavailable("Для получения результата тестирования, необходимо его завершить!");
+        } else {
+            logger.debug("Testing not done");
+            return Availability.unavailable(localeDataHelper.getLocaleMessage("shell.error.result"));
         }
     }
 }
