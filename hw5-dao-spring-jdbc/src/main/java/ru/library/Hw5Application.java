3b555f6d552cba964621948ffb527f50fdbb8dd7
Sergey Yakovlev <web-wind@ya.ru>
2021-01-08 22:36:24 +0300

    refactor

--- hw5-dao-spring-jdbc/src/main/java/ru/library/Hw5Application.java
+++ hw5-dao-spring-jdbc/src/main/java/ru/library/Hw5Application.java
@@ -1,10 +1,7 @@
 package ru.library;
 
-import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class Hw5Application {
